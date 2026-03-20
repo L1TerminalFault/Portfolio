2b79162dad122242538cac5819ae03104facc97f
x <x>
2026-03-20 12:32:50 +0300

    added hover follower in titlebar

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
                     <Image alt="" src={each.icon} className="size-6" />
                     {each.name}
 
-                    <div className="absolute translate-y-20 z-30 backdrop-blur-3xl group-hover:max-w-120 max-h-0 group-hover:max-h-40 max-w-0 rounded-2xl flex overflow-hidden group-hover:gap-4 group-hover:p-6 opacity-0 group-hover:opacity-100 bg-gray-900/90 transition-all duration-500 shadow-lg shadow-black/50">
+                    <div className="absolute max-md:left-0 translate-y-20 z-30 backdrop-blur-3xl group-hover:/ max-w-120 max-h-0 group-hover:max-h-40 max-w-0 rounded-2xl flex overflow-hidden gap-4 group-hover:p-6 opacity-0 group-hover:opacity-100 bg-gray-900/90 transition-all duration-300 shadow-lg shadow-black/50">
                       <div className="shrink-0">
                         <Image alt="" src={each.icon} className="size-18" />
                       </div>
--- src/app/components/TitleBar.tsx
+++ src/app/components/TitleBar.tsx
@@ -15,31 +15,60 @@ const routes = [
 export default function TitleBar() {
   const pathname = usePathname();
   const [updatePosition, setUpdatePosition] = useState(0);
+  const [hoverElement, setHoverElement] = useState<string | null>(null);
 
   useEffect(() => {
     window.addEventListener("resize", () => {
       setUpdatePosition(Math.random());
     });
+
+    let counter = 0;
+    const autoUpdaterId = setInterval(() => {
+      counter += 500;
+      setUpdatePosition(Math.random());
+      if (counter > 3000) clearInterval(autoUpdaterId);
+    }, 500);
     // return () => window.removeEventListener("resize", );
-  });
+  }, []);
 
   useEffect(() => {
     const currentElm = document.getElementById(pathname.split("/")[1]);
     const currentCord = currentElm?.getBoundingClientRect();
-    const follower = document.getElementById("follower");
+    const follower = document.getElementById("select-follower");
+    const hoverFollower = document.getElementById("hover-follower");
+
+    if (hoverFollower) {
+      hoverFollower.style.display = "block";
+
+      if (hoverElement) {
+        const hovered = document.getElementById(hoverElement);
+        const hoveredCoord = hovered?.getBoundingClientRect();
+
+        hoverFollower.style.left = `${hoveredCoord?.x}px`;
+        hoverFollower.style.width = `${hovered?.clientWidth}px`;
+        hoverFollower.style.display = "block";
+      } else {
+        hoverFollower.style.left = `${currentCord?.x}px`;
+        hoverFollower.style.width = `${currentElm?.clientWidth}px`;
+      }
+    }
 
     if (follower) {
       follower.style.left = `${currentCord?.x}px`;
       follower.style.width = `${currentElm?.clientWidth}px`;
       follower.style.display = "block";
     }
-  }, [pathname, updatePosition]);
+  }, [pathname, updatePosition, hoverElement]);
 
   return (
     <div className="absolute z-20 flex justify-center top-0 w-full p-3 #backdrop-blur-2xl">
       <div
-        id="follower"
-        className="absolute hidden md:p-4 p-3 rounded-lg mt-4 bg-white/10 border-t border-white/10 shadow shadow-gray-950 transition-all duration-500"
+        id="select-follower"
+        className="absolute hidden md:p-4 p-3 rounded-lg mt-4 bg-white/5 border-t border-white/10 shadow shadow-gray-950 transition-all duration-500"
+      ></div>
+      <div
+        id="hover-follower"
+        className="absolute hidden md:p-4 p-3 rounded-lg mt-4 bg-white/5 transition-all duration-500"
       ></div>
 
       <div className="flex sm:gap-6 gap-2 justify-around px-12 p-4 relative transition-all">
@@ -47,8 +76,10 @@ export default function TitleBar() {
           <Link
             key={route.name}
             id={route.name.toLowerCase()}
-            href={route.path}
-            className={`${pathname.includes(route.path) ? "text-white /underline underline-offset-5" : "text-gray-400 hover:bg-white/5"} transition-all opacity-0 duration-200 md:px-4 px-3 active:p-2 max-md:text-xs p-1 rounded-lg //hover:bg-white/10`}
+            href={pathname.includes(route.path) ? "" : route.path}
+            className={`${pathname.includes(route.path) ? "text-white /underline underline-offset-5" : "text-gray-400 /hover:bg-white/5"} transition-all opacity-0 duration-200 md:px-4 px-3 active:px-2 max-md:text-xs p-1 rounded-lg //hover:bg-white/10`}
+            onMouseEnter={() => setHoverElement(route.name.toLowerCase())}
+            onMouseLeave={() => setHoverElement(null)}
           >
             {route.name}
           </Link>
