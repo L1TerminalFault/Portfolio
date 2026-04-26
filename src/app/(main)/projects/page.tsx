f7357303d3796bd88a7a3b9eadfcfb14e1bcff77
x <x>
2026-04-26 13:00:09 +0300

    ui change

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -32,7 +32,7 @@ export default function Projects() {
               className={`relative flex flex-col w-full h-max gap-4 ${projects.find((project) => eachCat.index === project.category) ? "" : "hidden"}`}
             >
               <div id="reference"></div>
-              <div className="text-gray-400 2xl:pl-50 pl-14 flex gap-2 z-40 items-center top-0 sticky">
+              <div className="text-gray-400 2xl:pl-50 pl-14 flex px-5 py-2 rounded-r-full backdrop-blur-xl bg-linear-to-r from-transparent to-white/5 gap-2 z-40 items-center top-0 sticky">
                 {eachCat.text}
                 <GoDotFill className="text-[6px] text-center" />
                 {
